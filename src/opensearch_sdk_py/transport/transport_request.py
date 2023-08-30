fe91f2e6e84515ea02867889ebb6d2ed4f174ed4
Daniel Widdis <widdis@gmail.com>
2023-08-29 21:50:18 -0700

    TransportMessage temporary implementation
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/opensearch_sdk_py/transport/transport_request.py
+++ src/opensearch_sdk_py/transport/transport_request.py
@@ -1,15 +1,21 @@
 from opensearch_sdk_py.transport.stream_input import StreamInput
 from opensearch_sdk_py.transport.stream_output import StreamOutput
+from opensearch_sdk_py.transport.task_id import TaskId
 
 class TransportRequest:
-    def __init__(self):
+    def __init__(self, data:bytes):        
         super().__init__()
+        # TODO: Implementing this as a temporary way of getting writeable content from subclasses
+        # to enable end-to-end testing.  This will probably be replaced by passing the bytes 
+        # directly to write_to.  Will write test classes once that's done.
+        self.bytes = bytes
+        self.parent_task_id = TaskId()
 
     def read_from(self, input: StreamInput):
-        # parentTaskId = TaskId.readFromStream(in);
-        pass
+        self.parent_task_id = TaskId()
+        self.parent_task_id.read_from(input)
 
     def write_to(self, output: StreamOutput):
-        # parentTaskId.writeTo(out);
-        pass
+        self.parent_task_id.write_to(output)
+        output.write(self.bytes)
 
