cefb68d6186b2bfb546b71e3d75bb70919a876c6
Daniel Widdis <widdis@gmail.com>
2023-09-01 16:20:57 -0700

    DiscoveryExtensionNode and ExtensionDependency objects
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/opensearch_sdk_py/transport/discovery_extension_node.py
+++ src/opensearch_sdk_py/transport/discovery_extension_node.py
@@ -1,19 +1,39 @@
+from opensearch_sdk_py.transport.extension_dependency import ExtensionDependency
 from opensearch_sdk_py.transport.stream_input import StreamInput
 from opensearch_sdk_py.transport.stream_output import StreamOutput
 from opensearch_sdk_py.transport.discovery_node import DiscoveryNode
+from opensearch_sdk_py.transport.transport_address import TransportAddress
+from opensearch_sdk_py.transport.version import Version
 
 class DiscoveryExtensionNode(DiscoveryNode):
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self,
+                 node_name: str='',
+                 node_id: str='',
+                 address: TransportAddress=None,
+                 attributes: dict[str, str]=dict(),
+                 version: Version=Version(),
+                 minimum_compatible_version: Version=Version(),
+                 dependencies: list[ExtensionDependency]=[]):
+        super().__init__(node_name=node_name,
+                         node_id=node_id,
+                         address=address,
+                         attributes=attributes,
+                         version=version)
+        self.minimum_compatible_version = minimum_compatible_version
+        self.dependencies = dependencies
 
     def read_from(self, input: StreamInput):
-        # minimumCompatibleVersion = in.readVersion();
-        # int size = in.readVInt();
-        # dependencies = new ArrayList<>(size);
-        # for (int i = 0; i < size; i++) {
-        #     dependencies.add(new ExtensionDependency(in));
-        # }
-        pass
+        super().read_from(input)
+        self.minimum_compatible_version = input.read_version()
+        self.dependencies = []
+        num_dependencies = input.read_v_int()
+        for d in range(num_dependencies):
+            self.dependencies.append(ExtensionDependency().read_from(input))
+        return self
 
     def write_to(self, output: StreamOutput):
-        pass
+        super().write_to(output)
+        output.write_version(self.minimum_compatible_version)
+        output.write_v_int(len(self.dependencies))
+        for d in self.dependencies:
+            d.write_to(output)
---
+++ src/opensearch_sdk_py/transport/extension_dependency.py
@@ -0,0 +1,20 @@
+# https://github.com/opensearch-project/OpenSearch/blob/main/server/src/main/java/org/opensearch/extensions/ExtensionDependency.java
+
+from opensearch_sdk_py.transport.stream_input import StreamInput
+from opensearch_sdk_py.transport.stream_output import StreamOutput
+from opensearch_sdk_py.transport.version import Version
+
+class ExtensionDependency:
+    def __init__(self, unique_id: str='', version: Version=Version()):
+        self.unique_id = unique_id
+        self.version = version
+
+    def read_from(self, input: StreamInput):
+        self.unique_id = input.read_string()        
+        self.version = input.read_version()
+        return self
+
+    def write_to(self, output: StreamOutput):
+        output.write_string(self.unique_id)
+        output.write_version(self.version)
+ 
