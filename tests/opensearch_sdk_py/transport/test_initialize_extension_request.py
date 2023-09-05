b87c9e2dc72eaeb533d32c0dcf99ac8a881aa740
dblock <dblock@amazon.com>
2023-09-05 17:40:11 -0400

    Fix: test_initialize_extension_request.
    Signed-off-by: dblock <dblock@amazon.com>

--- tests/opensearch_sdk_py/transport/test_initialize_extension_request.py
+++ tests/opensearch_sdk_py/transport/test_initialize_extension_request.py
@@ -13,28 +13,24 @@ from opensearch_sdk_py.transport.transport_address import TransportAddress
 class TestInitializeExtensionRequest(unittest.TestCase):
     def test_initialize_extension_request(self):
         ier = InitializeExtensionRequest(
-            source_node=DiscoveryExtensionNode(
-                node_id="opensearch_node",
+            extension=DiscoveryExtensionNode(
+                node_id="extension_node",
                 address=TransportAddress("1.2.3.4", 1234, "foo.bar"),
             ),
-            extension=DiscoveryNode(
-                node_id="extension_node",
+            source_node=DiscoveryNode(
+                node_id="opensearch_node",
                 address=TransportAddress("5.6.7.8", 5678, "bar.baz"),
             ),
         )
+
         self.assertEqual(ier.source_node.node_id, "opensearch_node")
         self.assertEqual(ier.extension.node_id, "extension_node")
 
         output = StreamOutput()
         ier.write_to(output)
-        StreamInput(output.getvalue())
 
-        # FIXME: This read_from fails and I've spent hours debugging.
-        # There's an off-by-one byte associated with the TaskID that comes
-        # before this in the stream.
-        # Reading works fine for the actual content from OpenSearch.
-        # I'm not sure if there's a bug in it or what I'm doing here is an incorrect test
-        # Submitting now so I can get everything formatted and linted and will debug later
-        # ier = InitializeExtensionRequest().read_from(input)
-        # self.assertEqual(ier.source_node.node_id, 'opensearch_node')
-        # self.assertEqual(ier.extension.node_id, 'extension_node')
+        input = StreamInput(output.getvalue())
+        request = InitializeExtensionRequest()
+        ier = request.read_from(input)
+        self.assertEqual(ier.source_node.node_id, 'opensearch_node')
+        self.assertEqual(ier.extension.node_id, 'extension_node')
