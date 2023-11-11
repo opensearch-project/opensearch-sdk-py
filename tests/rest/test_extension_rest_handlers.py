23c4c76c91e60d02116ad57b15f63f6c00059ab3
Daniel Widdis <widdis@gmail.com>
2023-11-10 20:11:52 -0800

    Test multiple wildcards
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- tests/rest/test_extension_rest_handlers.py
+++ tests/rest/test_extension_rest_handlers.py
@@ -19,7 +19,7 @@ from opensearch_sdk_py.rest.rest_status import RestStatus
 
 
 class TestExtensionRestHandlers(unittest.TestCase):
-    class FakeRestHandler(ExtensionRestHandler):
+    class FooBarRestHandler(ExtensionRestHandler):
         def handle_request(self, rest_request: ExtensionRestRequest) -> ExtensionRestResponse:
             return ExtensionRestResponse(status=RestStatus.NOT_IMPLEMENTED)
 
@@ -27,29 +27,42 @@ class TestExtensionRestHandlers(unittest.TestCase):
         def routes(self) -> list[NamedRoute]:
             return [NamedRoute(RestMethod.GET, "/foo", "get_foo"), NamedRoute(RestMethod.GET, "/bar/{foo}", "get_bar")]
 
-    class DoubleFakeRestHandler(ExtensionRestHandler):
+    class BarFooRestHandler(ExtensionRestHandler):
         def handle_request(self, rest_request: ExtensionRestRequest) -> ExtensionRestResponse:
-            return ExtensionRestResponse(status=RestStatus.NOT_IMPLEMENTED)
+            return ExtensionRestResponse(status=RestStatus.OK)
 
         @property
         def routes(self) -> list[NamedRoute]:
-            return [NamedRoute(RestMethod.GET, "/bar/{baz}", "get_baz")]
+            return [NamedRoute(RestMethod.GET, "/bar/{baz}", "get_baz"), NamedRoute(RestMethod.GET, "/foo/{bar}/baz/{qux}", "get_barqux")]
 
-    def test_registers_handler(self) -> None:
+    def test_register_handlers(self) -> None:
         handlers = ExtensionRestHandlers()
-        handlers.register(TestExtensionRestHandlers.FakeRestHandler())
+        handlers.register(TestExtensionRestHandlers.FooBarRestHandler())
         self.assertEqual(len(handlers), 2)
-        self.assertIsInstance(handlers["GET /foo"], TestExtensionRestHandlers.FakeRestHandler)
-        self.assertIsInstance(handlers["GET /bar/*"], TestExtensionRestHandlers.FakeRestHandler)
+        self.assertIsInstance(handlers["GET /foo"], TestExtensionRestHandlers.FooBarRestHandler)
+        self.assertIsInstance(handlers["GET /bar/*"], TestExtensionRestHandlers.FooBarRestHandler)
         self.assertListEqual(handlers.named_routes, ["GET /foo get_foo", "GET /bar/{foo} get_bar"])
-
         response = handlers.handle("GET /foo", ExtensionRestRequest())
         self.assertEqual(response.status, RestStatus.NOT_IMPLEMENTED)
-
         response = handlers.handle("GET /bar/anything", ExtensionRestRequest())
         self.assertEqual(response.status, RestStatus.NOT_IMPLEMENTED)
-
         error = r"Can not find a matching route for GET /baz."
         self.assertRaisesRegex(Exception, error, handlers.handle, "GET /baz", ExtensionRestRequest)
+
+    def test_register_wildcard_handlers(self) -> None:
+        handlers = ExtensionRestHandlers()
+        handlers.register(TestExtensionRestHandlers.BarFooRestHandler())
+        self.assertEqual(len(handlers), 2)
+        self.assertIsInstance(handlers["GET /bar/*"], TestExtensionRestHandlers.BarFooRestHandler)
+        self.assertIsInstance(handlers["GET /foo/*/baz/*"], TestExtensionRestHandlers.BarFooRestHandler)
+        self.assertListEqual(handlers.named_routes, ["GET /bar/{baz} get_baz", "GET /foo/{bar}/baz/{qux} get_barqux"])
+        response = handlers.handle("GET /bar/anything", ExtensionRestRequest())
+        self.assertEqual(response.status, RestStatus.OK)
+        response = handlers.handle("GET /foo/something/baz/anything", ExtensionRestRequest())
+        self.assertEqual(response.status, RestStatus.OK)
+
+    def test_register_conflicts(self) -> None:
+        handlers = ExtensionRestHandlers()
+        handlers.register(TestExtensionRestHandlers.FooBarRestHandler())
         error = r"Can not register GET /bar/{baz}, GET /bar/\* is already registered."
-        self.assertRaisesRegex(Exception, error, handlers.register, TestExtensionRestHandlers.DoubleFakeRestHandler())
+        self.assertRaisesRegex(Exception, error, handlers.register, TestExtensionRestHandlers.BarFooRestHandler())
