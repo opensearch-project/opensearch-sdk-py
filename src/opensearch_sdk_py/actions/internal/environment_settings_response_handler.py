49f2c34d0e2847ef3f6c4704bdcddd729c211d90
Daniel Widdis <widdis@gmail.com>
2023-11-19 16:44:44 -0800

    Settings Transport Request/Response
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

---
+++ src/opensearch_sdk_py/actions/internal/environment_settings_response_handler.py
@@ -0,0 +1,31 @@
+#
+# Copyright OpenSearch Contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+#
+
+import logging
+
+from opensearch_sdk_py.actions.request_response_handler import RequestResponseHandler
+from opensearch_sdk_py.actions.response_handler import ResponseHandler
+from opensearch_sdk_py.transport.environment_settings_response import EnvironmentSettingsResponse
+from opensearch_sdk_py.transport.outbound_message_request import OutboundMessageRequest
+from opensearch_sdk_py.transport.stream_input import StreamInput
+from opensearch_sdk_py.transport.stream_output import StreamOutput
+
+
+class EnvironmentSettingsResponseHandler(ResponseHandler):
+    def __init__(self, next_handler: RequestResponseHandler) -> None:
+        self.next_handler = next_handler
+
+    def handle(self, request: OutboundMessageRequest, input: StreamInput) -> StreamOutput:
+        env_response = EnvironmentSettingsResponse().read_from(input)
+        logging.debug(f"< {env_response}")
+        if env_response.status:
+            return self.next_handler.send()
+        else:
+            # TODO error handling
+            return None
---
+++ src/opensearch_sdk_py/transport/extension_transport_request.py
@@ -0,0 +1,45 @@
+#
+# Copyright OpenSearch Contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+#
+
+# https://github.com/opensearch-project/OpenSearch/blob/main/server/src/main/java/org/opensearch/extensions/ExtensionRequest.java
+
+from typing import Optional
+
+from opensearch_sdk_py.protobuf.ExtensionRequestProto_pb2 import ExtensionRequest
+from opensearch_sdk_py.transport.request_type import RequestType
+from opensearch_sdk_py.transport.stream_input import StreamInput
+from opensearch_sdk_py.transport.stream_output import StreamOutput
+from opensearch_sdk_py.transport.transport_request import TransportRequest
+
+
+class ExtensionTransportRequest(TransportRequest):
+    def __init__(
+        self,
+        request_type: "RequestType",
+        unique_id: Optional[str] = None,
+    ) -> None:
+        super().__init__()
+        self.er = ExtensionRequest()
+        self.er.requestType = request_type.value
+        if unique_id is not None:
+            self.er.identity.uniqueId = unique_id
+
+    def read_from(self, input: StreamInput) -> "ExtensionTransportRequest":
+        super().read_from(input)
+        er_bytes = input.read_bytes(input.read_v_int())
+        self.er = ExtensionRequest()
+        self.er.ParseFromString(er_bytes)
+        return self
+
+    def write_to(self, output: StreamOutput) -> "ExtensionTransportRequest":
+        super().write_to(output)
+        er_bytes = self.er.SerializeToString()
+        output.write_v_int(len(er_bytes))
+        output.write(er_bytes)
+        return self
