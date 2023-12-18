48788a65de0002adb749586a422c100366cff378
Daniel Widdis <widdis@gmail.com>
2023-12-17 22:21:10 -0800

    Add coverage
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- tests/settings/test_setting.py
+++ tests/settings/test_setting.py
@@ -9,14 +9,19 @@
 
 import unittest
 
+from opensearch_sdk_py.settings.parser.integer_parser import IntegerParser
 from opensearch_sdk_py.settings.setting import Setting
 from opensearch_sdk_py.settings.settings import Settings
 
 
 class TestSetting(unittest.TestCase):
-    def test_settings(self) -> None:
+    def test_int_settings(self) -> None:
         settings = Settings()
         s = Setting.int_setting("answer.to.life", 24)
         self.assertEqual(s.get(settings), 24)
         settings = Settings({"answer.to.life": "42"})
         self.assertEqual(s.get(settings), 42)
+
+        self.assertRaises(ValueError, Setting.int_setting, "key", 10, 0, 5)
+        self.assertRaises(ValueError, Setting.int_setting, "key", 0, IntegerParser.MIN_VALUE - 1)
+        self.assertRaises(ValueError, Setting.int_setting, "key", 0, 0, IntegerParser.MAX_VALUE + 1)
