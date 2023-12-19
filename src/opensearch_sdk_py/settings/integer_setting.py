da17bc79b568b1280092113089dfaeb7160387b7
Daniel Widdis <widdis@gmail.com>
2023-12-18 20:38:22 -0800

    Refactor IntegerParser into IntegerSetting
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/opensearch_sdk_py/settings/integer_setting.py
+++ src/opensearch_sdk_py/settings/integer_setting.py
@@ -8,18 +8,51 @@
 #
 
 
-from opensearch_sdk_py.settings.parser.integer_parser import IntegerParser
 from opensearch_sdk_py.settings.setting import Setting
 from opensearch_sdk_py.settings.setting_property import SettingProperty
 from opensearch_sdk_py.settings.setting_type import SettingType
+from opensearch_sdk_py.transport.stream_input import StreamInput
+from opensearch_sdk_py.transport.stream_output import StreamOutput
 
 
 class IntegerSetting(Setting):
-    def __init__(self, key: str, default_value: int, min_value: int = IntegerParser.MIN_VALUE, max_value: int = IntegerParser.MAX_VALUE, *properties: SettingProperty) -> None:
-        if not (IntegerParser.MIN_VALUE <= min_value <= IntegerParser.MAX_VALUE):
+    MIN_VALUE: int = -(2**31)
+    MAX_VALUE: int = 2**31 - 1
+
+    def __init__(self, key: str, default_value: int, min_value: int = MIN_VALUE, max_value: int = MAX_VALUE, *properties: SettingProperty) -> None:
+        if not (IntegerSetting.MIN_VALUE <= min_value <= IntegerSetting.MAX_VALUE):
             raise ValueError("min_value must be within signed 32-bit integer range")
-        if not (IntegerParser.MIN_VALUE <= max_value <= IntegerParser.MAX_VALUE):
+        if not (IntegerSetting.MIN_VALUE <= max_value <= IntegerSetting.MAX_VALUE):
             raise ValueError("max_value must be within signed 32-bit integer range")
-        if not (min_value <= default_value <= max_value):
-            raise ValueError("default_value must be within signed 32-bit integer range and between min_value and max_value")
-        super().__init__(SettingType.INTEGER, key, lambda s: str(default_value), None, IntegerParser(min_value, max_value, key, properties), None, properties)
+        parser: IntegerSetting.Parser = IntegerSetting.Parser(min_value, max_value, key, properties)
+        parser.bounds_check(default_value)
+        super().__init__(SettingType.INTEGER, key, lambda s: str(default_value), None, parser, None, properties)
+
+    class Parser:
+        def __call__(self, s: str) -> int:
+            return self.bounds_check(int(s))
+
+        def __init__(self, min_value: int, max_value: int, key: str, *properties: SettingProperty) -> None:
+            self.min_value = min_value
+            self.max_value = max_value
+            self.key = key
+            self.is_filtered: bool = SettingProperty.FILTERED in properties
+
+        def bounds_check(self, value: int) -> int:
+            if not (self.min_value <= value <= self.max_value):
+                raise ValueError("Value for setting [" + self.key + "], must be between " + str(self.min_value) + " and " + str(self.max_value))
+            return value
+
+        def read_from(self, input: StreamInput) -> "IntegerSetting.Parser":
+            self.min_value = input.read_int()
+            self.max_value = input.read_int()
+            self.key = input.read_string()
+            self.is_filtered = input.read_boolean()
+            return self
+
+        def write_to(self, output: StreamOutput) -> "IntegerSetting.Parser":
+            output.write_int(self.min_value)
+            output.write_int(self.max_value)
+            output.write_string(self.key)
+            output.write_boolean(self.is_filtered)
+            return self
--- tests/settings/test_integer_setting.py
+++ tests/settings/test_integer_setting.py
@@ -10,8 +10,10 @@
 import unittest
 
 from opensearch_sdk_py.settings.integer_setting import IntegerSetting
-from opensearch_sdk_py.settings.parser.integer_parser import IntegerParser
+from opensearch_sdk_py.settings.setting_property import SettingProperty
 from opensearch_sdk_py.settings.settings import Settings
+from opensearch_sdk_py.transport.stream_input import StreamInput
+from opensearch_sdk_py.transport.stream_output import StreamOutput
 
 
 class TestSetting(unittest.TestCase):
@@ -23,5 +25,36 @@ class TestSetting(unittest.TestCase):
         self.assertEqual(s.get(settings), 42)
 
         self.assertRaises(ValueError, IntegerSetting, "key", 10, 0, 5)
-        self.assertRaises(ValueError, IntegerSetting, "key", 0, IntegerParser.MIN_VALUE - 1)
-        self.assertRaises(ValueError, IntegerSetting, "key", 0, 0, IntegerParser.MAX_VALUE + 1)
+        self.assertRaises(ValueError, IntegerSetting, "key", 0, IntegerSetting.MIN_VALUE - 1)
+        self.assertRaises(ValueError, IntegerSetting, "key", 0, 0, IntegerSetting.MAX_VALUE + 1)
+
+    def test_parser(self) -> None:
+        parser = IntegerSetting.Parser(0, 100, "foo", SettingProperty.FILTERED)
+        self.assertEqual(parser.min_value, 0)
+        self.assertEqual(parser.max_value, 100)
+        self.assertEqual(parser.key, "foo")
+        self.assertTrue(parser.is_filtered)
+
+    def test_parser_read_write(self) -> None:
+        parser = IntegerSetting.Parser(0, 100, "foo", SettingProperty.DYNAMIC, SettingProperty.NODE_SCOPE)
+        self.assertEqual(parser.min_value, 0)
+        self.assertEqual(parser.max_value, 100)
+        self.assertEqual(parser.key, "foo")
+        self.assertFalse(parser.is_filtered)
+
+        output = StreamOutput()
+        parser.write_to(output)
+        input = StreamInput(output.getvalue())
+        parser = IntegerSetting.Parser(0, 0, "").read_from(input)
+
+        self.assertEqual(parser.min_value, 0)
+        self.assertEqual(parser.max_value, 100)
+        self.assertEqual(parser.key, "foo")
+        self.assertFalse(parser.is_filtered)
+
+    def test_parsing(self) -> None:
+        parser = IntegerSetting.Parser(0, 100, "foo")
+        self.assertEqual(parser("42"), 42)
+        self.assertRaises(ValueError, parser, "not an integer")
+        self.assertRaises(ValueError, parser, -1)
+        self.assertRaises(ValueError, parser, 101)
