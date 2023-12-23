989a4b2043bb8be629f6b869cdc85fa4221d98e8
Daniel Widdis <widdis@gmail.com>
2023-12-22 17:09:03 -0800

    Add Boolean Setting implementation
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

---
+++ src/opensearch_sdk_py/settings/boolean_setting.py
@@ -0,0 +1,22 @@
+#
+# Copyright OpenSearch Contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+#
+
+
+from opensearch_sdk_py.settings.setting import Setting
+
+
+class BooleanSetting(Setting):
+    def __init__(self, key: str, default_value: bool, *properties: Setting.Property) -> None:
+        parser: BooleanSetting.Parser = BooleanSetting.Parser()
+        super().__init__(Setting.Type.BOOLEAN, key, lambda s: str(default_value), None, parser, None, properties)
+
+    class Parser:
+        def __call__(self, s: str) -> bool:
+            # match Java behavior: True (ignoring case) is true, otherwise false
+            return s.lower() == "true"
